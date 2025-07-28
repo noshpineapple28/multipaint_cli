4b513408e3b50543b05e256af8ade344cfd313ca
noshpineapple28 <manoucherin@gmail.com>
2025-07-28 17:46:46 -0400

    feat: linters and formatters added

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # multipaint_cli
+
 paint app for the CLI that allows multiple users connecting via websockets
+
+# Running
+
+First, ensure that you initialize the node project as well as make sure that node is installed.
+
+```sh
+# Can be run to check if node is installed
+node -v
+
+# installs dependencies
+npm install
+```
+
+To run the application:
+
+```sh
+# to run the application
+npm run start
+
+# to format (this will check for formatting issues and overwrite them)
+npm run format
+
+# to lint run
+npm run lint
+```
+
+# Pre-Commit Hooks
+
+In order to ensure a high quality of code coverage, there is pre-commit hooks installed that ensures all code is formatted correctly and linted before allowing it to enter any branch.
+
+Part of this process first checks that the code being run is properly formatted, if it isn't it is suggested that you run `npm run format` to correct changes, and then add changed files to your commit. It then runs `eslint` to ensure code is up to standard. If either process fails, your commit will fail.
---
+++ index.ts
@@ -0,0 +1,94 @@
+import { box, list, screen, Widgets } from 'blessed';
+import { terminal as term } from 'terminal-kit';
+
+// Create a screen with mouse support
+const scr: Widgets.Screen = screen({
+  mouse: true, // Enable mouse support
+});
+term.grabInput({ mouse: 'motion' });
+
+// list
+const menu: Widgets.ListElement = list({
+  width: '100%',
+  height: '30%',
+  xi: '0',
+  yi: '0',
+});
+
+// Create a box
+const box1: Widgets.BoxElement = box({
+  parent: menu,
+  left: '0%',
+  width: '30%',
+  height: '20%',
+  content: 'Move your mouse and click inside the box!',
+  tags: true,
+  border: {
+    type: 'line',
+  },
+  style: {
+    fg: 'white',
+    border: {
+      fg: '#f0f0f0',
+    },
+  },
+});
+box({
+  parent: menu,
+  left: 'center',
+  width: '40%',
+  height: '30%',
+  content: 'Move your mouse and click inside the box!',
+  tags: true,
+  border: {
+    type: 'line',
+  },
+  style: {
+    fg: 'white',
+    border: {
+      fg: '#f0f0f0',
+    },
+  },
+});
+box({
+  parent: menu,
+  left: '70%',
+  width: '30%',
+  height: '20%',
+  content: 'Move your mouse and click inside the box!',
+  tags: true,
+  border: {
+    type: 'line',
+  },
+  style: {
+    fg: 'white',
+    border: {
+      fg: '#f0f0f0',
+    },
+  },
+});
+
+// Append the box to the screen
+scr.append(menu);
+
+// terminal kit mouse events cus blessed's don't wanna work :(
+term.on('mouse', (name: string, data: { x: number; y: number }) => {
+  if (name === 'MOUSE_MOTION') {
+    menu.left = `${data.x}`;
+    menu.top = `${data.y}`;
+  }
+
+  if (name === 'MOUSE_LEFT_BUTTON_PRESSED') box1.style.bg = 'red';
+  else if (name === 'MOUSE_RIGHT_BUTTON_PRESSED')
+    box1.setContent('Mouse clicked!');
+  else box1.style.bg = 'black';
+  scr.render();
+});
+
+// Quit on Escape, q, or Control-C
+scr.key(['escape', 'q', 'C-c'], () => {
+  return process.exit(0);
+});
+
+// Render the screen
+scr.render();
